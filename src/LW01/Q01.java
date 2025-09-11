9f75f5242d5707429db48e50456146a4ba2773de
Harshani Pramodya <harshanip03@gmail.com>
2025-09-11 16:14:25 +0530

    answer for Q1

--- src/LW01/Q01.java
+++ src/LW01/Q01.java
@@ -1,4 +1,23 @@
 package LW01;
 
+import java.util.Scanner;
+
+
 public class Q01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an odd-length word: ");
+        String word = scanner.nextLine();
+
+        if (word.length() % 2 == 0) {
+            System.out.println("Error: The word must have an odd number of characters.");
+        } else {
+            int middleIndex = word.length() / 2;
+            char middleChar = word.charAt(middleIndex);
+            System.out.println("The middle character is: " + middleChar);
+        }
+
+        scanner.close();
+    }
 }
