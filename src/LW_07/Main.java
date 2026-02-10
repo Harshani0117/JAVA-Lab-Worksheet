17d3545193c2e4e127d2c58847504e92815c37fa
Harshani0117 <harshanip03@gmail.com>
2026-02-10 22:03:50 +0530

    LW07/08 answers

---
+++ src/LW_07/Main.java
@@ -0,0 +1,10 @@
+
+    package LW_07;
+
+    public class Main {
+        public static void main(String[] args) {
+            RegistrationForm registrationForm = new RegistrationForm();
+            registrationForm.setVisible(true);
+        }
+    }
+
